921c20935cb6aa115b5db918e174351d039a319e
kovvdn <40830091+kovvdn@users.noreply.github.com>
2021-02-01 00:42:37 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  //   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  devServer: {
+    port: 4200,
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({ template: "./src/tmpl/index.html" }),
+    new CopyPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+};
